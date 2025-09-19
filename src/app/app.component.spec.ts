fbba02d8c3c81a6ba87c872181085a05cc3b21ba
IGOR CHEPELEV <chepelevigor1@gmail.com>
2025-09-19 16:36:15 +0800

    init vercel

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mini-shop-second' title`, () => {
+  it(`should have the 'mini-shop' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mini-shop-second');
+    expect(app.title).toEqual('mini-shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mini-shop-second');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mini-shop');
   });
 });
